d0e137dd3afdab72083501c07e3a1e75ef4a7cc1
Pedro <pedro_freires18@outlook.com>
2024-12-02 21:53:13 -0300

    feat: entrypoint of application created

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask
-from Database.database import PostgresPool
-
-app = Flask(
-    "ImagesCaptureRouter"
-)
-postgres_pool = PostgresPool()
+from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
