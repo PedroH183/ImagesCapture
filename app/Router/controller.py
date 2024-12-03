43500e1ab68f820bf5a8419358fef500ce3ba1d7
Pedro <pedro_freires18@outlook.com>
2024-12-02 21:54:35 -0300

    feat: implemented basic controllers

---
+++ app/Router/controller.py
@@ -0,0 +1,28 @@
+import sys
+import logging
+from flask import Blueprint, jsonify, request
+from app.Database import repository
+
+bp = Blueprint('controller', __name__)
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger('flask.app')
+
+@bp.route("/ping", methods=["GET"])
+def ping():
+    return "pong", 200
+
+@bp.route("/all-images", methods=["GET"])
+def get_all_images():
+    """ This controller is to take all data of images downloaded """
+
+    data = request.get_json()
+
+    all_file_list = repository.get_all_files()
+    logger.info(f"All files >> {all_file_list}")
+
+    return "", 200
+
+@bp.route("/get-image", methods=["GET"])
+def get_image_download():
+    """ This controller take a base64 image """
+    pass
