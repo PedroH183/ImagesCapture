14b73b61d2e4287f9a6b6ac66f89af0260685aae
Pedro <pedro_freires18@outlook.com>
2024-12-07 21:24:51 -0300

    chore: created new folder structure to code

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-from app.Database import database
-from app.Router.controller import bp
-
-app = Flask(__name__)
-postgres_pool = database.PostgresPool()
-
-app.register_blueprint(bp)
